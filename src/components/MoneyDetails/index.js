87352f69152f0136c5f9185b75a0e135d20c8f3e
sanjay <sanjaysulochanap@gmail.com>
2021-12-16 21:46:01 +0530

    changed 1

--- src/components/MoneyDetails/index.js
+++ src/components/MoneyDetails/index.js
@@ -1,1 +1,46 @@
 // Write your code here
+import './index.css'
+
+const MoneyDetails = props => {
+  const {balance, income, expenses} = props
+
+  return (
+    <ul className="ulContainer">
+      <li className="listItemBg1">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/balance-image.png"
+          className="imgStyle"
+          alt="balance"
+        />
+        <div className="balanceContainer">
+          <p className="balancePara">Your Balance</p>
+          <p className="amountPara">Rs {balance}</p>
+        </div>
+      </li>
+      <li className="listItemBg2">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/income-image.png"
+          className="imgStyle"
+          alt="income"
+        />
+        <div className="balanceContainer">
+          <p className="balancePara">Your Balance</p>
+          <p className="amountPara">Rs {income}</p>
+        </div>
+      </li>
+      <li className="listItemBg3">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/expenses-image.png"
+          className="imgStyle"
+          alt="expenses"
+        />
+        <div className="balanceContainer">
+          <p className="balancePara">Your Balance</p>
+          <p className="amountPara">Rs {expenses}</p>
+        </div>
+      </li>
+    </ul>
+  )
+}
+
+export default MoneyDetails
--- src/components/MoneyManager/index.js
+++ src/components/MoneyManager/index.js
@@ -1,3 +1,7 @@
+import {Component} from 'react'
+import MoneyDetails from '../MoneyDetails'
+import './index.css'
+
 const transactionTypeOptions = [
   {
     optionId: 'INCOME',
@@ -10,3 +14,90 @@ const transactionTypeOptions = [
 ]
 
 // Write your code here
+
+class MoneyManager extends Component {
+  state = {
+    titleText: '',
+    amountText: '',
+    optionId: 'INCOME',
+    historyList: [],
+    balance: 0,
+    income: 0,
+    expenses: 0,
+  }
+
+  onChangeTitle = event => {
+    this.setState({titleText: event.target.value})
+  }
+
+  onChangeAmount = event => {
+    this.setState({amountText: event.target.value})
+  }
+
+  render() {
+    const {balance, income, expenses, titleText, amountText} = this.state
+    return (
+      <div className="bgContainer">
+        <div className="headerStyle">
+          <h1 className="heading">Hi, Richard</h1>
+          <p className="para">
+            Welcome back to your{' '}
+            <span className="heighlighted">Money Manager</span>
+          </p>
+        </div>
+        <div>
+          <div>
+            <MoneyDetails
+              balance={balance}
+              income={income}
+              expenses={expenses}
+            />
+          </div>
+        </div>
+        <div>
+          <div className="inputContainer">
+            <form className="formContainer">
+              <h2>Add Transaction</h2>
+              <label htmlFor="title" className="labelFormat">
+                TITLE
+              </label>
+              <input
+                id="title"
+                type="text"
+                placeholder="TITLE"
+                className="inputBox"
+                value={titleText}
+                onChange={this.onChangeTitle}
+              />
+              <label htmlFor="amount" className="labelFormat">
+                AMOUNT
+              </label>
+              <input
+                id="amount"
+                type="text"
+                placeholder="AMOUNT"
+                className="inputBox"
+                value={amountText}
+                onChange={this.onChangeAmount}
+              />
+              <label htmlFor="type" className="labelFormat">
+                TYPE
+              </label>
+              <select className="inputBox">
+                <option>Income</option>
+                <option>Expenses</option>
+              </select>
+              <button type="submit" className="addButton">
+                Add
+              </button>
+            </form>
+          </div>
+          <div className="historyContainer">
+            <p>History</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+export default MoneyManager
